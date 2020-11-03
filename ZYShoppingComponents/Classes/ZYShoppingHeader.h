e1f9021967e4b156c278d005e3e0231d02d2435b
xiaoyang521style <mailyzhao@163.com>
2020-11-03 14:29:04 +0800

    first commit

---
+++ ZYShoppingComponents/Classes/ZYShoppingHeader.h
@@ -0,0 +1,13 @@
+//
+//  ZYShoppingHeader.h
+//  Pods
+//
+//  Created by Apple on 2020/11/3.
+//
+
+#ifndef ZYShoppingHeader_h
+#define ZYShoppingHeader_h
+#define APPScreenHeight [[UIScreen mainScreen] bounds].size.height
+#define APPScreenWidth [[UIScreen mainScreen] bounds].size.width
+
+#endif /* ZYShoppingHeader_h */
